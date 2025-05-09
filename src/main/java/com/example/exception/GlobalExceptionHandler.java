b3aa998e66366a7984e15e1b6a4b5de60d456566
NIghtBird78 <panbogdan05@gmail.com>
2025-05-09 13:54:53 +0200

    exc handler

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -9,12 +9,14 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.lang.IllegalArgumentException;
+
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(Exception ex) {
         log.error("Bad request: {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
