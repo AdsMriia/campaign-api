4dd52af7cbd38ce837dc80ac39f3262d08c9669a
vladislavmosuyk <tokenvlad@gmail.com>
2025-05-17 13:28:50 +0200

    fix: Удален лишний перенос строки перед аннотацией @PreAuthorize в методе update класса ConstructorControllerImpl для улучшения читаемости кода.

--- src/main/java/com/example/controller/impl/ConstructorControllerImpl.java
+++ src/main/java/com/example/controller/impl/ConstructorControllerImpl.java
@@ -33,14 +33,14 @@ public class ConstructorControllerImpl implements ConstructorController {
     private final MessageService messageService;
 
     @Override
-    @PreAuthorize("hasAuthority('SPECIAL:SUPER_ADMIN') || hasAuthority('SPECIAL:OWNER')")
+    @PreAuthorize("hasAnyAuthority('SPECIAL:SUPER_ADMIN', 'SPECIAL:OWNER')")
     public MessageDto getById(@PathVariable("id") UUID id) {
         log.info("Получен запрос на получение креатива с ID: {}", id);
         return messageService.getById(id);
     }
 
     @Override
-    @PreAuthorize("hasAuthority('SPECIAL:SUPER_ADMIN') || hasAuthority('SPECIAL:OWNER')")
+    @PreAuthorize("hasAnyAuthority('SPECIAL:SUPER_ADMIN', 'SPECIAL:OWNER')")
     public Page<MessageDto> getAllByType(
             @PathVariable UUID workspaceId,
             @RequestParam(required = false) MessageStatus status,
@@ -52,15 +52,14 @@ public class ConstructorControllerImpl implements ConstructorController {
     }
 
     @Override
-    @PreAuthorize("hasAuthority('SPECIAL:SUPER_ADMIN') || hasAuthority('SPECIAL:OWNER')")
-
+    @PreAuthorize("hasAnyAuthority('SPECIAL:SUPER_ADMIN', 'SPECIAL:OWNER')")
     public MessageDto update(@RequestBody @Valid CreateMessageDto object, @PathVariable("id") UUID id) {
         log.info("Получен запрос на обновление креатива с ID: {}, новые данные: {}", id, object);
         return messageService.update(id, object);
     }
 
     @Override
-    @PreAuthorize("hasAuthority('SPECIAL:SUPER_ADMIN') || hasAuthority('SPECIAL:OWNER')")
+    @PreAuthorize("hasAnyAuthority('SPECIAL:SUPER_ADMIN', 'SPECIAL:OWNER')")
     public MessageDto create(
             @RequestParam("workspaceId") UUID workspaceId,
             @RequestBody @Valid CreateMessageDto createMessageDto) {
@@ -69,7 +68,7 @@ public class ConstructorControllerImpl implements ConstructorController {
     }
 
     @Override
-    @PreAuthorize("hasAuthority('SPECIAL:SUPER_ADMIN') || hasAuthority('SPECIAL:OWNER')")
+    @PreAuthorize("hasAnyAuthority('SPECIAL:SUPER_ADMIN', 'SPECIAL:OWNER')")
     public ResponseEntity<Void> delete(@PathVariable("id") UUID id) {
         log.info("Получен запрос на удаление креатива с ID: {}", id);
         messageService.delete(id);
